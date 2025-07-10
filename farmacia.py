d835b439497abc07f3ba637fc896f7d883e23f1f
ALUMNO <caxajgabriel2@gmail.com>
2025-07-09 19:25:42 -0600

    funciones par aagregar medicamentos y mostrar medicamentos

--- farmacia.py
+++ farmacia.py
@@ -9,4 +9,28 @@ class Medicamento:
     def get_medicamento(self):
         return f"Medicamento: {self.__nombre} Dosis: {self.__dosis} Presentacion: {self.__presentacion}, Con receta: {self.__receta_requerida} Precio: {self.__precio}"
 
-def agregarmedicamento():
+listaMedicamentos = []
+
+def agregarMedicamento():
+    nombre = input("Ingresa el nombre del medicamento: ")
+    while True:
+        try:
+            dosis = int(input("Ingresa la dosis del medicamento (gramos): "))
+            break
+        except ValueError:
+            print("El valor es incorrecto")
+    presentacion = input("Ingresa la presentacion del medicamento: ")
+    receta_requerida = input("El medicamento requiere receta medica?(Si,No): ")
+    while True:
+        try:
+            precio = int(input("Ingresa la precio del medicamento: "))
+            break
+        except ValueError:
+            print("El valor es incorrecto")
+
+    nuevoMedicamento = Medicamento(nombre, dosis, presentacion, receta_requerida, precio)
+    listaMedicamentos.append(nuevoMedicamento)
+
+def mostrarMedicamentos():
+    for medicamento in listaMedicamentos:
+        print(medicamento.get_medicamento())
