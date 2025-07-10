1e4eaf7da4ca968e9e93298a4e2f7749e3a0efb1
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-07-09 18:53:35 -0600

    MENU

--- Act4.py
+++ Act4.py
@@ -1,1 +1,8 @@
-print("--- FARMACIA ---")
+##MENU
+ def Menu():
+   print("MENU - FARMACIA")
+   print("1. RECEPCION - CLIENTES")
+   print("2. AGREGAR - MEDICAMENTOS")
+   print("3. SALIR")
+   print("OPCION A ELEGIR: ")
+
