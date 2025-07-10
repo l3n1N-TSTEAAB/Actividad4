2593ca5c3718240df06ae911d5b72e18fee97d7a
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-07-09 19:14:27 -0600

    CREANDO FUNCION PRINCIPAL

--- Act.py
+++ Act.py
@@ -1,14 +1,22 @@
-##MENU
 def Menu():
    print("MENU - FARMACIA")
    print("1. RECEPCION - CLIENTES")
    print("2. AGREGAR - MEDICAMENTOS")
    print("3. SALIR")
-   print("OPCION A ELEGIR: ")
 
-   def Pacientes():
+class Pacientes():
       def __init__(self,nombre, dpi, edad, padecimiento):
          self.__nombre = nombre
          self.__dpi = dpi
          self.__edad = edad
          self.__padecimiento = padecimiento
+
+
+
+opcion = 0
+PilaPacientes = []
+while opcion != 3:
+   opcion = 0
+   Menu()
+   opcion = int(input("OPCION A ELEGIR: "))
+
