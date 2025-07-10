3fa54992b2e0c334e7bb6285ec3979768aaedd2f
ALUMNO <caxajgabriel2@gmail.com>
2025-07-09 19:16:51 -0600

    funcion  get medicamento de la clase

--- farmacia.py
+++ farmacia.py
@@ -1,12 +1,12 @@
 class Medicamento:
-    def __init__(self, nombre, dosis, presentacion, receta_requerida):
-        self.nombre = nombre
-        self.dosis = dosis
+    def __init__(self, nombre, dosis, presentacion, receta_requerida, precio):
+        self.__nombre = nombre
+        self.__dosis = dosis
         self.__presentacion = presentacion
-        self.receta_requerida = receta_requerida
+        self.__receta_requerida = receta_requerida
+        self.__precio = precio
 
     def get_medicamento(self):
-        receta = "Sí" if self.receta_requerida else "No"
-        return (f"{self.nombre} | {self.dosis} | {self.presentacion} | "
-                f"Receta: {receta}")
+        return f"Medicamento: {self.__nombre} Dosis: {self.__dosis} Presentacion: {self.__presentacion}, Con receta: {self.__receta_requerida} Precio: {self.__precio}"
 
+def agregarmedicamento():
